9c5fe3ffc2cd760c6957b92e75c0d24e08a386f1
Dyani <dyani70@gmail.com>
2024-02-27 23:35:15 +0900

    Initial Commit

---
+++ MakingView/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MakingView
+//
+//  Created by Dyani on 2/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MakingView/MakingViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MakingViewApp.swift
+//  MakingView
+//
+//  Created by Dyani on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MakingViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
