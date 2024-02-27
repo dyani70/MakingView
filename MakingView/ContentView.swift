820c6540e711b25729268a3e84a5a7187dcf4bf7
Dyani <dyani70@gmail.com>
2024-02-28 00:03:14 +0900

    Making View

--- MakingView/ContentView.swift
+++ MakingView/ContentView.swift
@@ -9,15 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        helloText
+        HelloText()
+    }
+    
+ 
+}
+
+//뷰 정리하기
+
+///1. 확장기능 사용 -- 해당하는 뷰 화면에서만 사용할때
+extension ContentView {
+    ///깔끔하게 뷰 보여주기
+    private var helloText: some View { //변수 선언
+        Text("안녕하세요")
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding([.vertical, .horizontal])
+    }
+    
+}
+
+///2. 새로운 뷰로 빼주기 -- 어디서나 재사용하기위해
+struct HelloText: View {
+    var body: some View {
+        Text("안녕하세요")
+            .foregroundColor(.white)
+            .font(.largeTitle)
+            .frame(maxWidth: .infinity)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+            .padding([.vertical, .horizontal])
     }
 }
+
+
 
 #Preview {
     ContentView()
