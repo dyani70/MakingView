a71e5ebf2dea340244716673df0735e65251f69a
Dyani <dyani70@gmail.com>
2024-03-03 23:41:19 +0900

    Binding 추가설명

--- MakingView/BindingSample.swift
+++ MakingView/BindingSample.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct BindingSample: View {
-    @State private var backgroundColor1: Color = .brown
+    @State private var backgroundColor1: Color = .blue
     @State private var text: String = ""
     
     var body: some View {
@@ -19,6 +19,7 @@ struct BindingSample: View {
             
             VStack {
                 TextField("텍스트필드도 바인딩을 쓰네요", text: $text)
+                //ChangeColorButton불러오는데 2컬러에 1인 blue를 넘겨받음 (양방향으로 연결)
                 ChangeColorButton(backgroundColor2: $backgroundColor1)
             }
         }
@@ -27,10 +28,11 @@ struct BindingSample: View {
 
 
 struct ChangeColorButton: View {
-    // 바꾸고 싶은게 백그라운드컬러1인데 2만 변경됌 이럴때 Binding사용, 1과 2를 연결
+    // 버튼을 눌러 백그라운드컬러1을 바꾸고싶은데 2가 변경됌 이럴때 Binding사용, 1과 2를 연결 (양방향 연결)
     @Binding var backgroundColor2: Color
     var body: some View {
         Button {
+            //버튼을 클릭하면 컬러2가 오렌지로 변경됌
             backgroundColor2 = .orange
         } label: {
             Text("배경 바꾸기")
@@ -38,6 +40,8 @@ struct ChangeColorButton: View {
                 .tint(.white)
                 .padding()
                 .background{
+//                    Color.cyan
+                    //버튼색도 배경과 통일하고 싶다면 위의 값이 아닌 아래값을 넣어준다
                     backgroundColor2
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
